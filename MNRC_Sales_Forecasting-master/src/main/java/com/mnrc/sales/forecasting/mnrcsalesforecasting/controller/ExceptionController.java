f41ad60a353ecc231623db415eb0493cc6e0aa07
Ganapathy <ganapathys.krishnan@tcs.com>
2019-11-13 20:18:55 -0500

    Added Custome Exception object to present Response object

--- MNRC_Sales_Forecasting-master/src/main/java/com/mnrc/sales/forecasting/mnrcsalesforecasting/controller/ExceptionController.java
+++ MNRC_Sales_Forecasting-master/src/main/java/com/mnrc/sales/forecasting/mnrcsalesforecasting/controller/ExceptionController.java
@@ -1,5 +1,6 @@
 package com.mnrc.sales.forecasting.mnrcsalesforecasting.controller;
 
+import com.mnrc.sales.forecasting.mnrcsalesforecasting.exception.ApplicationException;
 import com.mnrc.sales.forecasting.mnrcsalesforecasting.exception.ForecastingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,8 +9,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionController {
+
+    /**
+     * Handles the Exception thrown by Controller
+     * @param exception
+     * @return
+     */
     @ExceptionHandler(value = ForecastingException.class)
     public ResponseEntity<Object> exception(ForecastingException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+        ApplicationException applicationException = new ApplicationException();
+        applicationException.setExceptionCode("INVALID_REQUEST");
+        applicationException.setExceptionMessage(exception.getMessage());
+        return new ResponseEntity<>(applicationException, HttpStatus.BAD_REQUEST);
     }
 }
